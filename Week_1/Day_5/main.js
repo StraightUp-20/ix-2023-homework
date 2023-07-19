9f6563c141ee664b03edbaec9066bb3fc0dab741
Pythancial <faifeleonardo@gmail.com>
2023-07-19 15:36:00 +0100

    more day 5

--- Week_1/Day_5/main.js
+++ Week_1/Day_5/main.js
@@ -1,70 +1,66 @@
 class Task {
-    constructor(taskName) {
-        this.taskName = taskName
-    } 
-
-}
-
-
-class UI {
+    constructor(taskName, complete, id) {
+      this.taskName = taskName;
+      this.complete = complete;
+      this.id = id;
+    }
+  }
+  
+  class UI {
     constructor() {
-        this.tableBody = document.getElementById('table-body');
-        this.form = document.getElementById('form')
-
-        this.form.addEventListener('submit', (e) => this.formIsClicked(e));
-
-        this.tasks = []
+      this.tableBody = document.getElementById('table-body');
+      this.form = document.getElementById('form');
+      this.taskInput = document.getElementById('input-task');
+  
+      this.form.addEventListener('submit', (e) => this.formIsClicked(e));
+  
+      this.tasks = [];
     }
-
+  
     formIsClicked(e) {
-        e.preventDefault();
-        
-        if (this.taskName.value == " ") {
-            return; 
-        }
-
-    const task = new Task(this.taskName.value)
-    this.tasks.push(task)
-    // console.log(task)
-    
-    this.tasks.value = ""
-
+      e.preventDefault();
+  
+      // validation check to see the input has a value
+      if (this.taskInput.value == '') {
+        alert('Please enter a value');
+        return;
+      }
+      
+      // creates unique identifier/timestamp for data entries in a database
+      const id = new Date().getTime();
+      // const task is the instance for parent class called Task
+      const task = new Task(id, this.taskInput.value, false);
+      this.tasks.push(task);
+  
+      this.renderTaskTable();
+  
+      // this.tasks.value = '';
     }
-    // renderTaskTable() {
-    //     this.tableBody.innerHTML = []
-    //     for (let i = 0; i < tasks.length; i++) {
-    //         const tasks = tasks[i]
+  
+    renderTaskTable() {
+      this.tableBody.innerHTML = [];
+      for (let i = 0; i < this.tasks.length; i++) {
+        const task = this.tasks[i];
+  
+        // check day 5 library book for the rest
+        const tr = this.createTaskTableRow(task);
+        this.tableBody.appendChild(tr);
+      }
+    }
+  
+    createTaskTableRow(task) {
+      const tr = document.createElement('tr');
+  
+      const tdTask = document.createElement('td');
+      const tdComplete = document.createElement('td');
+      const tdActions = document.createElement('td');
+        
+      tdTask.innerHTML = task.taskName
+      tdComplete.innerHTML = task.complete
+      tdActions.innerHTML = task.id
+     
+    return tr;
+    }
+  }
 
-    //     const tr = document.createTaskTableRow();
-    //     this.tableBody.appendChild(tr);
-            
-    //     }
-    // }
-}
-    const ui = new UI();
-
-//     renderCustomers() {
-//     this.tableBody.innerHTML = [];
-
-//     for (let i = 0; i < listArray.length; i++) {
-//     const tr = document.createElement('tr');
-
-//     let task = document.createElement("td");
-
-//     task.innerHTML = listArray[i].task;
-
-//     tr.appendChild(task);
-
-//     this.tableBody.appendChild(tr);
-
-//         }
-//     }
-// }
-
-// let listArray = [];
-// listArray.push(List);
-
-// const ui = new UI();
-// ui.renderCustomers();
-
-   
+  const ui = new UI();
