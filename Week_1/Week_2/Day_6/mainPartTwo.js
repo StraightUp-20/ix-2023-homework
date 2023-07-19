6574088adaf931d8df255601ab3ecf2befa1058d
Pythancial <faifeleonardo@gmail.com>
2023-07-19 18:44:51 +0100

    added code for taskList Part Two

---
+++ Week_1/Week_2/Day_6/mainPartTwo.js
@@ -0,0 +1,100 @@
+class Task {
+    constructor(id, taskName, complete) {
+      this.id = id;
+      this.taskName = taskName;
+      this.complete = complete;
+    }
+  }
+  
+  class UI {
+    constructor() {
+      this.tableBody = document.getElementById('table-body');
+      this.form = document.getElementById('form');
+      this.taskInput = document.getElementById('input-task');
+  
+      this.form.addEventListener('submit', (e) => this.formIsClicked(e));
+  
+      this.tasks = [];
+    }
+  
+    formIsClicked(e) {
+      e.preventDefault();
+  
+  
+      // validation check to see the input has a value
+      if (this.taskInput.value == '') {
+        alert('Please enter a value');
+        return;
+      }
+  
+      const id = new Date().getTime();
+      const task = new Task(id, this.taskInput.value, false);
+      this.tasks.push(task);
+  
+      this.renderTaskTable();
+  
+      this.tasks.value = '';
+  
+    }
+  
+    renderTaskTable() {
+      this.tableBody.innerHTML = [];
+      for (let i = 0; i < this.tasks.length; i++) {
+        const task = this.tasks[i];
+  
+        const tr = this.createTaskTableRow(task);
+        this.tableBody.appendChild(tr);
+      }
+    }
+  
+    createTaskTableRow(task) {
+      const tr = document.createElement('tr');
+  
+      const tdTask = document.createElement('td');
+      const tdComplete = document.createElement('td');
+      const tdActions = document.createElement('td');
+  
+      tdTask.innerHTML = task.taskName;
+      // tdComplete.innerHTML = `<input type="radio" name="taskComplete_${task.id}" ${task.complete ? 'checked' : ''} enabled/>`;
+      // tdActions.innerHTML =  `<i class="fas fa-trash me-2" style="cursor: pointer;"></i><i class="fas fa-edit" style="cursor: pointer;"></i>`;
+  
+      const actionButtons = this.createActionButtons();
+      tdActions.appendChild(actionButtons[0])
+      tdActions.appendChild(actionButtons[1])
+  
+      tr.appendChild(tdTask);
+      tr.appendChild(tdComplete);
+      tr.appendChild(tdActions);
+  
+      return tr;
+  
+    }
+    createActionButtons(task) {
+        const deleteButton = document.createElement('button');
+        const editButton = document.createElement('button');
+  
+        deleteButton.setAttribute("class", "btn btn-danger btn-sm me-1");
+        deleteButton.innerHTML = "Delete";
+        deleteButton.addEventListener("click", () => {
+          // Call deletefunction()
+        })
+  
+        editButton.setAttribute("class", "btn btn-warning btn-sm ms-1");
+        editButton.innerHTML = "Edit"
+        editButton.addEventListener("click", () => {
+          // Call editfunction()
+    })
+  
+    return [deleteButton, editButton]
+  }
+  
+    // deleteFunction() {
+  
+    // }
+  
+    // editFunction() {
+  
+    // }
+  }
+  const ui = new UI();
+  
