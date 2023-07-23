2e6c74ef039992ed642dc72fa6d8227d347ab41c
Pythancial <faifeleonardo@gmail.com>
2023-07-23 23:45:52 +0100

    started learning about API's for poki assignment

---
+++ Week_2/Day_8/api/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors');
+
+const app express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello World"); 
+})
+
+app.get('/posts', (req, res) => {
+    res.send({
+        name: "Leonardo",
+        surname: "Faife",
+        age: "21",
+        title: "SWE"
+
+    })
+
+
+
+app.listen (() => {
+    console.log("Listening for port 3000")
+})
+
+}
