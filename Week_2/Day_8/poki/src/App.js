5f341d4a22ebd834581ecb07551068dc676a7650
Pythancial <faifeleonardo@gmail.com>
2023-07-24 17:34:28 +0100

    more frontend

--- Week_2/Day_8/poki/src/App.js
+++ Week_2/Day_8/poki/src/App.js
@@ -40,15 +40,15 @@ function App() {
 
       return (
       <div className=" container text-center mt-5">
-        <div className='card bg-body-secondary pt-3'>
+        <div className='card bg-body-secondary pt-3 p-4 '>
         <h1 className='text-center pb-5'>Poki DataBase </h1>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder='Input name of Pokimon'/>
-          <button class="btn btn-outline-primary" type="button" onClick={fetchPosts}>+</button>
+        <div class="input-group mb-3 ">
+          <input type="text" className="form-control" placeholder='Name of Pokimon goes here'/>
+          <button className="btn btn-outline-primary" type="button" onClick={fetchPosts}>+</button>
           </div>
           <div>
-          <input type="text" class="form-control" placeholder='Input name of Pokimon'/>
-          <button class="btn btn-outline-primary" type="button" onClick={fetchPosts}>+</button>
+          <input type="text" className="form-control" placeholder='Pokimons Ability...'/>
+          <button className="btn btn-outline-primary" type="button" onClick={fetchPosts}>+</button>
           </div>
           <div>
               {
