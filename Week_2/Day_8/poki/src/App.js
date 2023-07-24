46207a4aff6ac50a77fc2b0893de73a657fa981c
Pythancial <faifeleonardo@gmail.com>
2023-07-24 17:31:01 +0100

    Getting syntax correct for poki api

---
+++ Week_2/Day_8/poki/src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+
+import {useState} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+    const url = 'https://pokeapi.co/api/v2/'
+
+    const [posts, setPosts] = useState([])
+
+    async function fetchPosts() {
+     
+    const res = await fetch(url + '/posts', {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json"
+        
+      }
+      })
+
+      const data = await res.json()
+      setPosts(data);
+      console.log(data)
+
+    }
+
+      // async function fetchOurPost() {
+      //   const res = await fetch(url, 'posts', {
+      //     method: 'GET',
+      //     headers: {
+      //       "Content-Type": "application/json"
+          
+      //   }
+      //   })
+
+      // const data = await res.json()
+      // console.log(data)
+
+      // }
+
+      return (
+      <div className=" container text-center mt-5">
+        <div className='card bg-body-secondary pt-3'>
+        <h1 className='text-center pb-5'>Poki DataBase </h1>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder='Input name of Pokimon'/>
+          <button class="btn btn-outline-primary" type="button" onClick={fetchPosts}>+</button>
+          </div>
+          <div>
+          <input type="text" class="form-control" placeholder='Input name of Pokimon'/>
+          <button class="btn btn-outline-primary" type="button" onClick={fetchPosts}>+</button>
+          </div>
+          <div>
+              {
+                posts.map((x) => {
+                  return <div>
+                    {x.name}
+                    {x.ability}
+                  </div>
+                })}
+             </div>
+        </div>
+      </div>
+      )
+    }
+
+export default App; 
