8db4d6b15fa7341c1027a3610a0fe6d1b4eb13bf
Pythancial <faifeleonardo@gmail.com>
2023-07-25 18:59:28 +0100

    did 43 min worth of day_9 recording

---
+++ Week_2/Day_9/task-list/src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXzQa1diHEgi6AFrkfhGO4CDUXdw-LBG0",
+  authDomain: "task-list-day9.firebaseapp.com",
+  projectId: "task-list-day9",
+  storageBucket: "task-list-day9.appspot.com",
+  messagingSenderId: "41772928115",
+  appId: "1:41772928115:web:f9b81dc6e32e42a8926e06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {db};
